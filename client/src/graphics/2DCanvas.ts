3989c251a1ee0f119773a51d9f783f397bd743bb
Henri Willman <henri.t.willman@gmail.com>
2026-01-14 16:54:21 +0200

    remove canvas scaling

--- client/src/graphics/2DCanvas.ts
+++ client/src/graphics/2DCanvas.ts
@@ -3,24 +3,3 @@ export const canvas2d = canvas.getContext('2d', {
 	alpha: true,
 	desynchronized: true,
 })!;
-
-export function resizeHudCanvas() {
-	const dpr = Math.min(window.devicePixelRatio, 2);
-	const width = window.innerWidth;
-	const height = window.innerHeight;
-
-	canvas.width = Math.floor(width * dpr);
-	canvas.height = Math.floor(height * dpr);
-
-	canvas.style.width = width + 'px';
-	canvas.style.height = height + 'px';
-
-	canvas2d.setTransform(dpr, 0, 0, dpr, 0, 0);
-	canvas2d.imageSmoothingEnabled = false;
-}
-
-// Call once at startup
-resizeHudCanvas();
-
-// Call on resize
-window.addEventListener('resize', resizeHudCanvas);
